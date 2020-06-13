0b3cf0dcac02a203774620514316ce3b9f22a034
Dhyey Vachhani <dhyey890@gmail.com>
2020-06-13 22:01:58 +0530

    initial commit

---
+++ src/test/java/com/mazebank/mazebank/MazebankApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mazebank.mazebank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MazebankApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
